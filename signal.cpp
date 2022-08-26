8aa8de5266f12fdf222cb6510c370a25c2c60ddb
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-26 17:51:47 +0300

    [:signal] Enh | NL

--- signal.cpp
+++ signal.cpp
@@ -13,7 +13,6 @@ void arrout(const T &aIterable, std::size_t aSize, const char *aDelimiter = kDel
 	using ValueType = decltype(aIterable[0]);
 	std::size_t count = 0;
 	std::copy(&aIterable[0], &aIterable[aSize], std::ostream_iterator<ValueType>(std::cout, aDelimiter));
-	std::cout << std::endl << std::endl;
 }
 
 template <class T>
@@ -26,11 +25,11 @@ int testu16()
 {
 	static std::array<std::int16_t, 4096> sCoefficients;
 	static auto buf = signal;
-	arrouts(buf);
+//	arrouts(buf);
 	dsps_fft2r_init_sc16(sCoefficients.data(), sCoefficients.size());
 	dsps_fft2r_sc16_ansi_(buf.data(), kSignalLen, sCoefficients.data());
 	dsps_bit_rev_sc16_ansi(buf.data(), buf.size());
-	arrouts(buf);
+//	arrouts(buf);
 	dsps_fft2r_sc16_ansi_(buf.data(), kSignalLen, sCoefficients.data());
 	dsps_bit_rev_sc16_ansi(buf.data(), buf.size());
 	arrout(buf, kSignalLen * 2);
@@ -45,7 +44,7 @@ int testf32()
 	dsps_fft2r_init_fc32(NULL, 4096);
 	// Here you have your complex signal.
 
-	arrouts(signalf);
+//	arrouts(signalf);
 
 	// FFT calculated here
 	dsps_fft2r_fc32(y_cf, N);
@@ -54,7 +53,7 @@ int testf32()
 	// Here you have your complex FFT spectrum
 
 	// Here you can make your operations with complex spectrum at y_cf
-	arrouts(signalf);
+//	arrouts(signalf);
 
 	// FFT calculated here
 	dsps_fft2r_fc32(y_cf, N);
@@ -63,7 +62,7 @@ int testf32()
 	// Here you have your original complex signal in time domain.
 	// y_cf[i*2 + 0] == x1[i]*wind(i)*N;
 	// y_cf[i*2 + 1] == 0;
-	arrouts(signalf);
+//	arrouts(signalf);
 	// You can make multiplication with constant here:
 	dsps_mulc_f32(y_cf, y_cf, 2*N, 1.0f / N, 1, 1);
 	arrouts(signalf);
@@ -71,8 +70,8 @@ int testf32()
 
 int main()
 {
-//	testu16();
-	testf32();
+	testu16();
+//	testf32();
 
 	return 0;
 }
