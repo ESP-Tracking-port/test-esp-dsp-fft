a241b5f2a1fe9ca0165999784f9d6edaf5b4b86c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-26 16:55:57 +0300

    [:generate] Build

--- generate.py
+++ generate.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 
 import random
+import math
 
 
 FILE_SIGNAL = "signal.h"
 CEIL = 255
-SIGNAL_LEN = 20
+SIGNAL_LEN = 64
 NL = "\n"
 DNL = "\n\n"
 
 
 def real_gen_iter():
-	yield "#include <cstdint>" + DNL
-	yield "std::int16_t signal[] = {"  + NL
+	yield "#include <cstdint>" + NL
+	yield "#include <array>" + NL
+	yield NL
+	yield "using ValueType = std::int16_t" + NL
+	yield "static constexpr std::size_t kSignalLen = %d;" % SIGNAL_LEN
+	yield NL
+	yield "std::array<std::int16_t, kSignalLen * 2> signal {{" + NL
 
 	for i in range(SIGNAL_LEN):
-		yield '\t' + str(int(random.random() * CEIL)) + "," + NL  # Real part
+		# yield '\t' + str(int(random.random() * CEIL)) + "," + NL  # Real part
+		yield '\t' + str(int(math.cos(i) * CEIL)) + "," + NL  # Real part
 		yield '\t' + "0," + NL  # Imaginary part
 
-	yield "};" + NL
-	yield "static constexpr std::size_t kSignalLen = sizeof(signal) / sizeof(signal[0]) / 2;"
+	yield "}};" + NL
 
 
 def file_save(filename, content):
