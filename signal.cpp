e59833b6ddb8a49c07cb5cdaa9c48e6695a68ccc
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-26 17:19:19 +0300

    [:signal,signalf32] Impl | f32 example

--- signal.cpp
+++ signal.cpp
@@ -3,30 +3,21 @@
 #include <iterator>
 #include "esp_dsp.h"
 #include "signal.h"
+#include "signalf32.h"
 
 template <class T>
-void arrout(const T &aIterable, std::size_t aSize, std::size_t aStep = 1, const std::size_t aStartFrom = 0,
-	const char *aDelimiter = ", ")
+void arrout(const T &aIterable, std::size_t aSize, const char *aDelimiter = ", ")
 {
 	using ValueType = decltype(aIterable[0]);
 	std::size_t count = 0;
-
-	std::copy_if(&aIterable[0], &aIterable[aSize], std::ostream_iterator<ValueType>(std::cout, aDelimiter),
-		[&count, aStep, aStartFrom](const ValueType &)
-		{
-			const bool ret = count % aStep == 0 && count > aStartFrom;
-			++count;
-
-			return ret;
-		});
-
+	std::copy(&aIterable[0], &aIterable[aSize], std::ostream_iterator<ValueType>(std::cout, aDelimiter));
 	std::cout << std::endl << std::endl;
 }
 
 template <class T>
-void arrouts(const T &arr, std::size_t aStep = 1, const std::size_t aStartFrom = 0, const char *aDelimiter = ", ")
+void arrouts(const T &arr, const char *aDelimiter = ", ")
 {
-	arrout(arr.data(), arr.size(), aStep, aStartFrom, aDelimiter);
+	arrout(arr.data(), arr.size(), aDelimiter);
 }
 
 int testu16()
@@ -40,12 +31,46 @@ int testu16()
 	arrouts(buf);
 	dsps_fft2r_sc16_ansi_(buf.data(), kSignalLen, sCoefficients.data());
 	dsps_bit_rev_sc16_ansi(buf.data(), buf.size());
-	arrout(buf, kSignalLen * 2, 1);
+	arrout(buf, kSignalLen * 2);
 
 	return 0;
 }
 
+int testf32()
+{
+	const auto N = kSignalfLen;
+	float *y_cf = signalf.data();
+	dsps_fft2r_init_fc32(NULL, 4096);
+	// Here you have your complex signal.
+
+	arrouts(signalf);
+
+	// FFT calculated here
+	dsps_fft2r_fc32(y_cf, N);
+	// Bit reverse
+	dsps_bit_rev_fc32(y_cf, N);
+	// Here you have your complex FFT spectrum
+
+	// Here you can make your operations with complex spectrum at y_cf
+	arrouts(signalf);
+
+	// FFT calculated here
+	dsps_fft2r_fc32(y_cf, N);
+	// Bit reverse
+	dsps_bit_rev_fc32(y_cf, N);
+	// Here you have your original complex signal in time domain.
+	// y_cf[i*2 + 0] == x1[i]*wind(i)*N;
+	// y_cf[i*2 + 1] == 0;
+	arrouts(signalf);
+	// You can make multiplication with constant here:
+	dsps_mulc_f32(y_cf, y_cf, 2*N, 1.0f / N, 1, 1);
+	arrouts(signalf);
+}
+
 int main()
 {
-	return testu16();
+//	testu16();
+	testf32();
+
+	return 0;
 }
