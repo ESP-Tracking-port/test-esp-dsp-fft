01eb7fbfece55db4d92fa344901cb130b5d16a7d
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-26 16:56:16 +0300

    [:signal] Impl | Stable, still doesn't work

--- signal.cpp
+++ signal.cpp
@@ -4,8 +4,6 @@
 #include "esp_dsp.h"
 #include "signal.h"
 
-static std::int16_t sCoefficients[kSignalLen / 2];
-
 template <class T>
 void arrout(const T &aIterable, std::size_t aSize, std::size_t aStep = 1, const std::size_t aStartFrom = 0,
 	const char *aDelimiter = ", ")
@@ -13,7 +11,7 @@ void arrout(const T &aIterable, std::size_t aSize, std::size_t aStep = 1, const 
 	using ValueType = decltype(aIterable[0]);
 	std::size_t count = 0;
 
-	std::copy_if(&aIterable[0], &aIterable[aSize - 1], std::ostream_iterator<ValueType>(std::cout, aDelimiter),
+	std::copy_if(&aIterable[0], &aIterable[aSize], std::ostream_iterator<ValueType>(std::cout, aDelimiter),
 		[&count, aStep, aStartFrom](const ValueType &)
 		{
 			const bool ret = count % aStep == 0 && count > aStartFrom;
@@ -21,13 +19,33 @@ void arrout(const T &aIterable, std::size_t aSize, std::size_t aStep = 1, const 
 
 			return ret;
 		});
+
+	std::cout << std::endl << std::endl;
+}
+
+template <class T>
+void arrouts(const T &arr, std::size_t aStep = 1, const std::size_t aStartFrom = 0, const char *aDelimiter = ", ")
+{
+	arrout(arr.data(), arr.size(), aStep, aStartFrom, aDelimiter);
+}
+
+int testu16()
+{
+	static std::array<std::int16_t, 4096> sCoefficients;
+	static auto buf = signal;
+	arrouts(buf);
+	dsps_fft2r_init_sc16(sCoefficients.data(), sCoefficients.size());
+	dsps_fft2r_sc16_ansi_(buf.data(), kSignalLen, sCoefficients.data());
+	dsps_bit_rev_sc16_ansi(buf.data(), buf.size());
+	arrouts(buf);
+	dsps_fft2r_sc16_ansi_(buf.data(), kSignalLen, sCoefficients.data());
+	dsps_bit_rev_sc16_ansi(buf.data(), buf.size());
+	arrout(buf, kSignalLen * 2, 1);
+
+	return 0;
 }
 
 int main()
 {
-	dsps_fft2r_init_sc16(sCoefficients, kSignalLen / 2);
-	dsps_fft2r_sc16_ansi_(signal, kSignalLen / 2, sCoefficients);
-	arrout(signal, kSignalLen);
-
-	return 0;
+	return testu16();
 }
