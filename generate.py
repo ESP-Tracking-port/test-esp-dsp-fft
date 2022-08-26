8621d6b437bb7fe5fe74b3eda26e3b84065ff679
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-26 17:09:20 +0300

    [:generate] Build

--- generate.py
+++ generate.py
@@ -7,6 +7,7 @@ import os
 
 
 FILE_SIGNAL = "signal.h"
+FILE_SIGNAL_FLOAT = "signalf32.h"
 CEIL = 255
 SIGNAL_LEN = 64
 NL = "\n"
@@ -19,7 +20,8 @@ def file_append(appendix, fname):
 		mode = 0o774
 		path = pathlib.Path(str(fname)).parent.resolve()
 
-		if pathlib.Path(__file__).parent.resolve() != path and not os.path.exists(path):
+		if pathlib.Path(__file__).parent.resolve() != path \
+				and not os.path.exists(path):
 			os.makedirs(str(path), mode)
 
 		with open(fname, 'w') as f:
@@ -33,7 +35,8 @@ def real_gen_iter(typestr, suffix="", tp=int):
 	yield "#include <cstdint>" + NL
 	yield "#include <array>" + NL
 	yield NL
-	yield "static constexpr std::size_t kSignalLen = %d;" % SIGNAL_LEN
+	yield "static constexpr std::size_t kSignal%sLen = %d;" % (suffix,
+		SIGNAL_LEN)
 	yield NL
 	yield "std::array<%s, kSignalLen * 2> signal%s {{" % (typestr, suffix) + NL
 
@@ -47,6 +50,7 @@ def real_gen_iter(typestr, suffix="", tp=int):
 
 def main():
 	file_append(''.join(real_gen_iter("std::int16_t", "", int)), FILE_SIGNAL)
+	file_append(''.join(real_gen_iter("float", "f", float)), FILE_SIGNAL_FLOAT)
 
 
 if __name__ == "__main__":
